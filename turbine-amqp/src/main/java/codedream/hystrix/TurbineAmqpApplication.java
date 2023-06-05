efe1603d2abffd208e258acacf2ca858819b03be
Your Name <1552743651@qq.com>
2023-06-05 15:03:43 +0800

    20230605-commit

--- turbine-amqp/src/main/java/codedream/hystrix/TurbineAmqpApplication.java
+++ turbine-amqp/src/main/java/codedream/hystrix/TurbineAmqpApplication.java
@@ -3,6 +3,7 @@ package codedream.hystrix;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 import org.springframework.context.annotation.Configuration;
 
